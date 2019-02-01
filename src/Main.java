53d2428a05524101555f6401ebf85c5c4a75106f
wael <khelilwael@gmail.com>
2019-02-01 21:03:51 +0100

    add file reading methode

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        File inFile = null;
+        if (0 < args.length) {
+            inFile = new File(args[0]);
+            try {
+                String content = readFile(args[0], Charset.defaultCharset());
+                System.out.println(content);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            System.err.println("Invalid arguments count:" + args.length);
+        }
+    }
+    static String readFile(String path, Charset encoding) throws IOException {
+        byte[] encoded = Files.readAllBytes(Paths.get(path));
+        return new String(encoded, encoding);
     }
 }
